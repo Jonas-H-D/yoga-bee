c99778c6138270fbe47f166f864a18e2b150d2ae
Jonas HD <jonas.hermonduc@gmail.com>
2021-01-28 10:30:46 +0100

    new, create, show, index action and view created, newlocation form created

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,4 +1,26 @@
 class LocationsController < ApplicationController
   def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.new(location_params)
+    @location.save
+
+    redirect_to location
+  end
+
+  def index
+    @locations = Location.all
+  end
+
+  def show
+    @location = Location.find(params[:id])
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:name, :address, :city, :country, :number_of_rooms, :capacity, :price_per_night, :rating, :description)
   end
 end
